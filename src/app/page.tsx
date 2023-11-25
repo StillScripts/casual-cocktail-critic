ebe00171f13b130426dad462b9c7f858d4087e2a
StillScripts <80142739+StillScripts@users.noreply.github.com>
2023-11-25 15:42:08 +1100

    Make the UI more plain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import { CreateRecipe } from "@/app/_components/create-recipe";
 import { Recipies } from "@/app/_components/recipes";
 
 import { getServerAuthSession } from "@/server/auth";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const session = await getServerAuthSession();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-200">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
           Casual <span className="text-[hsl(280,100%,70%)]">Cocktail</span>
@@ -17,15 +18,14 @@ export default async function Home() {
         </h1>
         <div className="flex flex-col items-center gap-2">
           <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
+            <p className="text-center text-2xl text-black">
               {session && <span>Logged in as {session.user?.name}</span>}
             </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
+            <Button asChild>
+              <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+                {session ? "Sign out" : "Sign in"}
+              </Link>
+            </Button>
           </div>
         </div>
 
