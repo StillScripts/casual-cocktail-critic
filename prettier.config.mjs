6f42ca5e25f56d9aa0f7c6afbd903e4549d65f3a
StillScripts <80142739+StillScripts@users.noreply.github.com>
2023-12-02 16:12:05 +1100

    Add prettier preferences

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').options} */
 const config = {
+  endOfLine: "auto",
+  semi: false,
+  useTabs: true,
+  singleQuote: true,
+  arrowParens: "avoid",
+  tabWidth: 2,
+  trailingComma: "none",
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,19 +1,19 @@
-import { api } from "@/trpc/server";
-import { EditRecipeForm } from "@/app/_components/edit-recipe/form";
+import { api } from '@/trpc/server'
+import { EditRecipeForm } from '@/app/_components/edit-recipe/form'
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic'
 
 const EditRecipePage = async ({ params }: { params: { id: string } }) => {
-  const recipes = await api.recipe.getSingle.query({ id: parseInt(params.id) });
-  const recipe = recipes ? recipes[0] : null;
-  if (!recipe) {
-    return <h2>Recipe Not Found</h2>;
-  }
-  return (
-    <div className="container min-h-screen py-16">
-      <EditRecipeForm recipes={recipes} />
-    </div>
-  );
-};
+	const recipes = await api.recipe.getSingle.query({ id: parseInt(params.id) })
+	const recipe = recipes ? recipes[0] : null
+	if (!recipe) {
+		return <h2>Recipe Not Found</h2>
+	}
+	return (
+		<div className="container min-h-screen py-16">
+			<EditRecipeForm recipes={recipes} />
+		</div>
+	)
+}
 
-export default EditRecipePage;
+export default EditRecipePage
